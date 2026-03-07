64a8b19a56099cb7f1db35d0d582928edf05d09a
ydlech77 <p03420363@gmail.com>
2026-03-07 07:08:33 -1200

    fix BASE_DIR error

--- hospital_system/settings.py
+++ hospital_system/settings.py
@@ -10,15 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+from pathlib import Path
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ALLOWED_HOSTS = ['*']
 
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
